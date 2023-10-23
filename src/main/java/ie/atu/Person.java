83a911ce1a52fbb015c6596fd98adf6e559aed89
ChoonXiang <g00425056@atu.ie>
2023-10-23 16:40:12 +0100

    Added a new constructor

--- src/main/java/ie/atu/Person.java
+++ src/main/java/ie/atu/Person.java
@@ -3,6 +3,12 @@ public class Person {
     String firstName;
     String lastName;
     int age;
+    //Constructor
+    public Person(String firstName,String lastName,int age){
+        this.firstName=firstName;
+        this.lastName=lastName;
+        this.age=age;
+    }
     public void displayInfo(){
         System.out.println("name: "+firstName+" "+lastName+", Age: "+age);
     }
