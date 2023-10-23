cdfbcf61a1c15cd538e232b87a30efcbcdaefb21
ChoonXiang <g00425056@atu.ie>
2023-10-23 16:53:30 +0100

    Added a method getUserInput

--- src/main/java/ie/atu/Person.java
+++ src/main/java/ie/atu/Person.java
@@ -1,4 +1,5 @@
 package ie.atu;
+import java.util.Scanner;
 public class Person {
     String firstName;
     String lastName;
@@ -14,6 +15,16 @@ public class Person {
         this.firstName=firstName;
         this.lastName=lastName;
         this.age=age;
+    }
+    //Method to get user input
+    public void getUserInput(){
+        Scanner scanner=new Scanner(System.in);
+        System.out.print("Enter first name: ");
+        this.firstName=scanner.nextLine();
+        System.out.print("Enter last name: ");
+        this.lastName=scanner.nextLine();
+        System.out.print("Enter age: ");
+        this.age=scanner.nextInt();
     }
     public void displayInfo(){
         System.out.println("name: "+firstName+" "+lastName+", Age: "+age);
